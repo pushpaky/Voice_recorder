4edb765760503acdd8bf2645f907c8104abc067d
pushpakyelementure <pushpa@elementure.tech>
2024-05-13 11:05:09 +0530

    intial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import sounddevice
+from scipy.io.wavfile import write
+
+
+fs = 44100
+
+seconds = 20
+print("Recording....")
+record_voice = sounddevice.rec(int(seconds*fs), samplerate=fs, channels=2)
+
+sounddevice.wait()
+write("out.wav", fs, record_voice)
